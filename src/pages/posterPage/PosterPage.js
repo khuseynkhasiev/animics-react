c1f4de3b51ee17e7bea9252a2d00c34bd9cccf63
khuseyn-khasiev <khuseyn-khasiev@mail.ru>
2023-09-07 09:43:20 +0300

    добавлен favicon и неймниг страницы

--- src/pages/posterPage/PosterPage.js
+++ src/pages/posterPage/PosterPage.js
@@ -4,7 +4,7 @@ export default function Poster() {
         <div className="poster">
             <div className='poster__bg'>
                 <div className="poster__text">
-                    <h2 className="poster__title">TOBA</h2>
+                    <h1 className="poster__title">TOBA</h1>
                     <p className="poster__subtitle">Разработка интерактивного анимированного комикса</p>
                 </div>
             </div>
