342cbcec53491145faaab7b5f4b94d2250e559c3
khuseyn-khasiev <khuseyn-khasiev@mail.ru>
2023-09-27 10:19:24 +0300

    изменены размеры форм регистрации и обратной связи

--- src/pages/registerFinishPage/RegisterFinishPage.js
+++ src/pages/registerFinishPage/RegisterFinishPage.js
@@ -81,62 +81,64 @@ export default function RegisterFinishPage({ handleRegister }) {
         <div className='registerFinishPage'>
             <div className='registerFinishPage__container'>
                 <div className='registerFinishPage__formContainer'>
-                    <h2 className='registerFinishPage__title'>Финиш!</h2>
-                    <p className='registerFinishPage__subtitle'>Вы это сделали!</p>
-                    <div className="registerFinishPage__flexBox">
-                        <form className='registerFinishPage__form' id='registerFinishPage' action="" method="POST" onSubmit={handleSubmitForm}>
-                            <p className='registerFinishPage__titleText'>Когда Вы родились?</p>
-                            <input
-                                className='registerFinishPage__input'
-                                type="date"
-                                id='registerFinishPage__date'
-                                name='date'
-                                placeholder='дд.мм.гггг.'
-                                onChange={handleChangeValueInputDate}
-                                required
-                            />
-                            <div className="registerFinishPage__inputContainer">
+                    <div className='registerFinishPage__content'>
+                        <h2 className='registerFinishPage__title'>Финиш!</h2>
+                        <p className='registerFinishPage__subtitle'>Вы это сделали!</p>
+                        <div className="registerFinishPage__flexBox">
+                            <form className='registerFinishPage__form' id='registerFinishPage' action="" method="POST" onSubmit={handleSubmitForm}>
+                                <p className='registerFinishPage__titleText'>Когда Вы родились?</p>
                                 <input
-                                    className="registerFinishPage__inputCheckbox"
-                                    type="checkbox"
-                                    name="agreement"
-                                    id='agreement'
-                                    onChange={handleChangeValueInputAgreement}
+                                    className='registerFinishPage__input'
+                                    type="date"
+                                    id='registerFinishPage__date'
+                                    name='date'
+                                    placeholder='дд.мм.гггг.'
+                                    onChange={handleChangeValueInputDate}
+                                    required
                                 />
-                                <label className='registerFinishPage__labelCheckbox' htmlFor='agreement'>какое то очень длинное пользовательсоке соглашение о том что передаются и обрабатываются персональные данные</label>
+                                <div className="registerFinishPage__inputContainer">
+                                    <input
+                                        className="registerFinishPage__inputCheckbox"
+                                        type="checkbox"
+                                        name="agreement"
+                                        id='agreement'
+                                        onChange={handleChangeValueInputAgreement}
+                                    />
+                                    <label className='registerFinishPage__labelCheckbox' htmlFor='agreement'>какое то очень длинное пользовательсоке соглашение о том что передаются и обрабатываются персональные данные</label>
+                                </div>
+                                <div className="registerFinishPage__inputContainer">
+                                    <input
+                                        className="registerFinishPage__inputCheckbox"
+                                        type="checkbox"
+                                        name="consent"
+                                        id='consent'
+                                        onChange={handleChangeValueInputConsent}
+                                    />
+                                    <label className='registerFinishPage__labelCheckbox' htmlFor='consent'>даю согласие на то чтобы получать оповещения и рссылки на почту/телефон</label>
+                                </div>
+                                {errorTextActive && <p className='registerFinishPage__errorText'>Необходимо принять пользовательское соглашение!</p>}
+                                {errorTextDateActive && <p className='registerFinishPage__errorText'>Вам должно быть 14+ лет</p>}
+                            </form>
+                            <div className="registerFinishPage__qrBlock">
+                                <div className='registerFinishPage__qrBlockBg'></div>
+                                <p className="registerFinishPage__qrText">Осталось скачать наше приложение...</p>
+                                <p className="registerFinishPage__qrSubtext">погрузись в другую реальность...</p>
                             </div>
-                            <div className="registerFinishPage__inputContainer">
-                                <input
-                                    className="registerFinishPage__inputCheckbox"
-                                    type="checkbox"
-                                    name="consent"
-                                    id='consent'
-                                    onChange={handleChangeValueInputConsent}
-                                />
-                                <label className='registerFinishPage__labelCheckbox' htmlFor='consent'>даю согласие на то чтобы получать оповещения и рссылки на почту/телефон</label>
-                            </div>
-                            {errorTextActive && <p className='registerFinishPage__errorText'>Необходимо принять пользовательское соглашение!</p>}
-                            {errorTextDateActive && <p className='registerFinishPage__errorText'>Вам должно быть 14+ лет</p>}
-                        </form>
-                        <div className="registerFinishPage__qrBlock">
-                            <div className='registerFinishPage__qrBlockBg'></div>
-                            <p className="registerFinishPage__qrText">Осталось скачать наше приложение...</p>
-                            <p className="registerFinishPage__qrSubtext">погрузись в другую реальность...</p>
                         </div>
+                        <div className='registerFinishPage__btnContainer'>
+                            <button
+                                className='registerFinishPage__submit'
+                                type='button'
+                                onClick={handleBackPageClick}
+                            >Назад</button>
+                            <button
+                                className='registerFinishPage__submit'
+                                type='submit'
+                                form='registerFinishPage'
+                            >Завершить</button>
+                        </div>
+                        <div className='registerFinishPage__footer'></div>
                     </div>
-                    <div className='registerFinishPage__btnContainer'>
-                        <button
-                            className='registerFinishPage__submit'
-                            type='button'
-                            onClick={handleBackPageClick}
-                        >Назад</button>
-                        <button
-                            className='registerFinishPage__submit'
-                            type='submit'
-                            form='registerFinishPage'
-                        >Завершить</button>
-                    </div>
-                    <div className='registerFinishPage__footer'></div>
                 </div>
             </div>
         </div>
